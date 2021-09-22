784fb1d9b6b74a49eac93f2001935343ed1ea61b
pickforbass <56023752+pickforbass@users.noreply.github.com>
2021-09-22 13:56:55 +0200

    exercise 3

---
+++ auto-write-text/script2.js
@@ -0,0 +1,18 @@
+const text = "Hello world this is a autotype text ! ";
+let i = 0;
+
+setInterval(() => {
+    
+    document.body.innerHTML = text.slice(0,i);
+    i++;
+
+    if (i > text.length) {
+        setTimeout(() => {
+            i=0;
+            document.body.innerHTML = "";
+        
+        }, 700);
+        
+    }
+    
+}, 130);
